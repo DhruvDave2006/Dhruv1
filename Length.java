4886e4f7c58fe285db1190ae1e15019577ab0d2f
DhruvDave2006 <davedhruvv06@gmail.com>
2025-01-01 17:04:51 +0000

    modified

--- Length.java
+++ Length.java
@@ -2,7 +2,7 @@ class Length {
 
 	public static void main (String args[]){
 		String txt = "DhruvDave";
-		System.out.println("The length of the word is :" + txt.length());
+		System.out.println(txt);
 
 	}
 }
